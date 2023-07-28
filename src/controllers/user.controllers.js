61eae6dfb263b4b892a2a2d35de2cebf23da3979
bryanbreener <bquispevaleriano@gmail.com>
2023-07-28 10:56:10 -0500

    first commit

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,36 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    const user = await User.findAll()
+    return res.json(user)
+});
+const getOne = catchError(async(req, res) => {
+    const {id} = req.params
+    const user = await User.findByPk(id)
+    return res.json(user)
+});
+const create = catchError(async(req, res) => {
+    const userData = req.body
+    const user = await User.create(userData)
+    return res.status(201).json(user)
+});
+const remove = catchError(async(req, res) => {
+    const {id} = req.params
+    const user = await User.destroy({where:id == id})
+    return res.sendStatus(204)
+});
+const update = catchError(async(req, res) => {
+    const {id} = req.params
+    const data = req.body
+    const user = await User.update(data, {where: id == id})
+    return res.json(user[1][0])
+});
+
+module.exports = {
+    getAll,
+    getOne,
+    create,
+    remove,
+    update
+}
